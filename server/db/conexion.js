c97f5f94f91f3ec98576c7574041856ff76e3f6a
Maxi Guzmán <maxiguz17@gmail.com>
2025-12-09 17:21:00 -0300

    Update conexion.js
    Se quitaron las credenciales

--- server/db/conexion.js
+++ server/db/conexion.js
@@ -1,20 +1,20 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 // Configuración de la conexión a la base de datos MySQL
 const connection = mysql.createConnection({
-    host: 'localhost',      
-    user: 'root',          
-    password: '3617206Lila',           
+    host: 'localhost',
+    user: 'root',
+    password: 'su pass',
     database: 'ecommerce'
 });
 
 // Conectar a MySQL
 connection.connect((err) => {
     if (err) {
-        console.error('Error conectando a la base de datos:', err.stack);
+        console.error('Error conectando a la base de datos:', err);
         return;
     }
-    console.log('Conexión a la base de datos exitosa');
+    console.log('✔ Conexión a la base de datos exitosa');
 });
 
 module.exports = connection;
